d0d1893a3d7d17d1750009f41f5bf6a3dc292319
luo <luohengquan@yeah.net>
2021-12-12 19:02:36 +0800

    account添加功能

--- seata-demo/account-service/src/main/java/com/luo/demo/seata/controller/AccountController.java
+++ seata-demo/account-service/src/main/java/com/luo/demo/seata/controller/AccountController.java
@@ -2,12 +2,10 @@ package com.luo.demo.seata.controller;
 
 
 import com.luo.demo.sc.base.model.result.RespResult;
+import com.luo.demo.seata.model.entity.Account;
 import com.luo.demo.seata.service.IAccountService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -39,4 +37,30 @@ public class AccountController {
     RespResult debit(@RequestParam("userId") String userId, @RequestParam("money") BigDecimal money) {
         return accountService.debit(userId, money);
     }
+
+    @GetMapping("/detail")
+    RespResult<Account> getAccount(@RequestParam Long id) {
+        log.info("查询用户信息，参数：{}", id);
+        Account account = accountService.getById(id);
+        log.info("查询用户详情，结果：{}", account);
+        return RespResult.successData(account);
+    }
+
+    @GetMapping("/detail/{id}")
+    RespResult<Account> getAccountWithId(@PathVariable Long id) {
+        log.info("查询用户信息，path参数：{}", id);
+        Account account = accountService.getById(id);
+        log.info("查询用户详情，结果：{}", account);
+        return RespResult.successData(account);
+    }
+
+    @PostMapping("/add")
+    RespResult addAccount(@RequestBody Account account) {
+        log.info("新增用户，参数：{}", account);
+        Boolean result = accountService.save(account);
+        log.info("新增用户，结果：{}", result);
+        return RespResult.ofBoolean(result);
+    }
+
+
 }
--- seata-demo/account-service/src/main/java/com/luo/demo/seata/service/impl/AccountServiceImpl.java
+++ seata-demo/account-service/src/main/java/com/luo/demo/seata/service/impl/AccountServiceImpl.java
@@ -1,12 +1,11 @@
 package com.luo.demo.seata.service.impl;
 
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.luo.demo.sc.base.execption.MsgRuntimeException;
 import com.luo.demo.sc.base.model.result.RespResult;
-import com.luo.demo.seata.model.entity.Account;
 import com.luo.demo.seata.mapper.AccountMapper;
+import com.luo.demo.seata.model.entity.Account;
 import com.luo.demo.seata.service.IAccountService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
